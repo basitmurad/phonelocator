f33f79bf2efaf91e5ca41ff855ef9c5898868cf0
basitmurad <basitmurad7@gmail.com>
2024-12-17 22:11:35 +0500

    First commit

---
+++ app/src/main/java/com/example/locationtracker/DeviceNameActivity.kt
@@ -0,0 +1,28 @@
+package com.example.locationtracker
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.locationtracker.databinding.ActivityDeviceNameBinding
+
+class DeviceNameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDeviceNameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize the binding object
+        binding = ActivityDeviceNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set default text to TextView
+
+    }
+
+    fun backButton(view: View) {
+
+        finish()
+
+    }
+}
